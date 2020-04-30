28feede5c93bc46424882b2b480fa893868b085c
febritoriqkhul <febritoriqkhul@gmail.com>
2020-04-30 10:58:42 +0700

    Create new file config.js in folder src/firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBApUXsz_fgzTbI7gxnBX9F4N76SUt57NA",
+    authDomain: "fir-login-540c8.firebaseapp.com",
+    databaseURL: "https://fir-login-540c8.firebaseio.com",
+    projectId: "fir-login-540c8",
+    storageBucket: "fir-login-540c8.appspot.com",
+    messagingSenderId: "62666151091",
+    appId: "1:62666151091:web:40da63f1dbfd521832ccc6",
+    measurementId: "G-T6R7WCB2C3"
+};
+
+export default firebaseConfig;
